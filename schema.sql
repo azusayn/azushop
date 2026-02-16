24498789e8c718294bca847e55f243c28a6190b7
Azusain <azusaings@gmail.com>
2026-02-16 23:31:29 +0800

    feat(schema): create table products

--- schema.sql
+++ schema.sql
@@ -4,3 +4,16 @@ CREATE TABLE users (
   password_hash VARCHAR(255) NOT NULL,
   salt VARCHAR(255) NOT NULL
 )
+
+CREATE TABLE products(
+  id BIGSERIAL PRIMARY KEY,
+  product_name VARCHAR(255) NOT NULL,
+  seller_id INT NOT NULL,
+  stock_quantity INT NOT NULL CHECK (stock_quantity >= 0),
+  unit_price NUMERIC(10, 2) NOT NULL CHECK (unit_price >= 0),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (seller_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_products_seller_id ON products(seller_id);
