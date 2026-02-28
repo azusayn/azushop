0666002e53ae3da626f1e7999c8fbc3adb132b0c
Azusain <azusaings@gmail.com>
2026-02-28 20:34:46 +0800

    feat(inventory_service): implement Inventory Service
    AdjustStock()
    BatchGetStock()
    ReserveStock()
    ReleaseStock()
    DeductStock()

--- internal/data/inventory.go
+++ internal/data/inventory.go
@@ -3,9 +3,12 @@ package data
 import (
 	"azushop/internal/biz"
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type InventoryRepo struct {
@@ -16,8 +19,12 @@ func NewInventoryRepo(data *Data) biz.InventoryRepo {
 	return &InventoryRepo{data: data}
 }
 
-func (r *InventoryRepo) UpdateInventories(ctx context.Context, inventories []*biz.Inventory, paths []string) error {
-	gormClient := r.data.gormClient
+// updates the same set of fields (defined by paths) for each inventory.
+func (repo *InventoryRepo) UpdateInventories(ctx context.Context, inventories []*biz.Inventory, paths []string) error {
+	gormClient := GetTransaction(ctx)
+	if gormClient == nil {
+		gormClient = repo.data.gormClient.WithContext(ctx)
+	}
 	for _, inv := range inventories {
 		vals := map[string]interface{}{}
 		for _, path := range paths {
@@ -31,22 +38,112 @@ func (r *InventoryRepo) UpdateInventories(ctx context.Context, inventories []*bi
 		if len(vals) == 0 {
 			return errors.New("no valid paths")
 		}
-		if err := gormClient.WithContext(ctx).Model(inv).Updates(vals).Error; err != nil {
+		if err := gormClient.Model(inv).Updates(vals).Error; err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (r *InventoryRepo) BatchGetInventories(ctx context.Context, skuIDs []uuid.UUID) ([]*biz.Inventory, error) {
+func (repo *InventoryRepo) BatchGetInventories(ctx context.Context, skuIDs []uuid.UUID) ([]*biz.Inventory, error) {
 	if len(skuIDs) == 0 {
 		return nil, errors.New("empty skuIDs")
 	}
-	gormClient := r.data.gormClient
+	gormClient := repo.data.gormClient
 	var inventories []*biz.Inventory
 	err := gormClient.WithContext(ctx).Where("sku_id IN ?", skuIDs).Find(&inventories).Error
 	if err != nil {
 		return nil, err
 	}
 	return inventories, nil
+}
+
+func (repo *InventoryRepo) UpdateDeltaQuantity(ctx context.Context, inventories []*biz.Inventory, paths []string) error {
+	gormClient := GetTransaction(ctx)
+	if gormClient == nil {
+		gormClient = repo.data.gormClient
+	}
+	for _, inventory := range inventories {
+		m := make(map[string]any)
+		for _, path := range paths {
+			switch path {
+			case "stock_quantity":
+				m[path] = gorm.Expr("stock_quantity + ?", inventory.StockQuantity)
+			case "reserved_quantity":
+				m[path] = gorm.Expr("reserved_quantity + ?", inventory.ReservedQuantity)
+			default:
+				return fmt.Errorf("invalid path %q", path)
+			}
+		}
+		if err := gormClient.
+			WithContext(ctx).
+			Model(&biz.Inventory{}).
+			Where("sku_id = ?", inventory.ID).
+			Updates(m).Error; err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (repo *InventoryRepo) GetInventoryLock(ctx context.Context, orderID int64) (*biz.InventoryLock, error) {
+	gormClient := GetTransaction(ctx)
+	if gormClient == nil {
+		gormClient = repo.data.gormClient
+	}
+	var inventoryLock biz.InventoryLock
+	if err := gormClient.WithContext(ctx).Where("order_id = ?", orderID).Find(&inventoryLock).Error; err != nil {
+		return nil, err
+	}
+	return &inventoryLock, nil
+}
+
+func (repo *InventoryRepo) CreateInventoryLock(
+	ctx context.Context,
+	orderID int64,
+	payload map[uuid.UUID]int64,
+	status biz.InventoryLockStatus,
+) error {
+	gormClient := GetTransaction(ctx)
+	if gormClient == nil {
+		gormClient = repo.data.gormClient
+	}
+	payloadJSON, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	lock := &biz.InventoryLock{
+		OrderID: orderID,
+		Payload: payloadJSON,
+		Status:  status,
+	}
+	return gormClient.WithContext(ctx).Table("inventory_lock").Create(lock).Error
+}
+
+func (repo *InventoryRepo) UpdateInventoryLock(ctx context.Context, inventoryLocks []*biz.InventoryLock, paths []string) error {
+	gormClient := GetTransaction(ctx)
+	if gormClient == nil {
+		gormClient = repo.data.gormClient
+	}
+	for _, invLock := range inventoryLocks {
+		m := make(map[string]any)
+		for _, path := range paths {
+			switch path {
+			case "payload":
+				m[path] = invLock.Payload
+			case "status":
+				m[path] = invLock.Status
+			default:
+				return fmt.Errorf("invalid path %q", path)
+			}
+		}
+		if err := gormClient.
+			WithContext(ctx).
+			Model(&biz.InventoryLock{}).
+			Where("order_id = ?", invLock.OrderID).
+			Updates(m).Error; err != nil {
+			return err
+		}
+	}
+	return nil
 }
